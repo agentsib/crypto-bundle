9af5fcbd3dc880a71dbbbf290f59366352743df0
Denis Davydov <thereisnobugs@gmail.com>
2024-09-05 15:13:43 +0300

    Missed dependencies and test fixes

--- tests/Functional/app/FirstCase/bundles.php
+++ tests/Functional/app/FirstCase/bundles.php
@@ -1,6 +1,7 @@
 <?php
 
 return [
+    new \Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
     new \Symfony\Bundle\FrameworkBundle\FrameworkBundle(),
     new \AgentSIB\CryptoBundle\AgentSIBCryptoBundle()
 ];
